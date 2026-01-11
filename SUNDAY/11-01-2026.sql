ee4e7339c0511b3083d8b0b934bed23a8e77003b
Human-Gechi <kimberlydaniel363@gmail.com>
2026-01-12 00:01:39 +0100

    Adding

--- SUNDAY/11-01-2026.sql
+++ SUNDAY/11-01-2026.sql
@@ -8,6 +8,7 @@ SELECT
     CONCAT(EXTRACT(YEAR FROM SHIPMENT_DATE), '-', EXTRACT(MONTH FROM SHIPMENT_DATE)) AS YEAR_MONTH
 FROM AMAZON_SHIPMENT
 GROUP BY YEAR_MONTH;
+
 /*Find the average number of bathrooms and bedrooms for each city’s property types.
 Output the result along with the city name and the property type.
 COMPANY : AIRBNB
@@ -34,3 +35,27 @@ SELECT company,
 FROM forbes_global_2010_2014
 JOIN maxprofits ON forbes_global_2010_2014.profits = maxprofits.max_profit
 WHERE sector = 'Financials';
+
+/*Count the number of user events performed by MacBookPro users.
+Output the result along with the event name.
+Sort the result based on the event count in the descending order
+COMPANY :APPLE
+*/
+SELECT
+    COUNT(USER_ID),
+    EVENT_NAME
+FROM PLAYBOOK_EVENTS
+WHERE DEVICE = 'macbook pro'
+GROUP BY EVENT_NAME
+ORDER BY COUNT(USER_ID) DESC;
+
+/*Find the inspection date and risk category (pe_description) of facilities named 'STREET CHURROS' that received a score below 95
+COMPANY : City of Los Angeles
+*/
+SELECT
+    ACTIVITY_DATE,
+    PE_DESCRIPTION
+FROM LOS_ANGELES_RESTAURANT_HEALTH_INSPECTIONS
+WHERE FACILITY_NAME = 'STREET CHURROS' AND SCORE < 95
+GROUP BY  PE_DESCRIPTION, ACTIVITY_DATE
+
