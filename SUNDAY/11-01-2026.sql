2ded545fa152d66677658ae7636997a69f5543ee
Human-Gechi <kimberlydaniel363@gmail.com>
2026-01-11 14:11:58 +0100

    queries:sunday

--- SUNDAY/11-01-2026.sql
+++ SUNDAY/11-01-2026.sql
@@ -1,10 +1,36 @@
 /*Write a query that will calculate the number of shipments per month.
 The unique key for one shipment is a combination of shipment_id and sub_id.
-Output the year_month in format YYYY-MM and the number of shipments in that month.*/
-
+Output the year_month in format YYYY-MM and the number of shipments in that month.
+COMPANY : AMAZON
+*/
 SELECT
     COUNT(CONCAT(SHIPMENT_ID, SUB_ID)) as shipment_count,
     CONCAT(EXTRACT(YEAR FROM SHIPMENT_DATE), '-', EXTRACT(MONTH FROM SHIPMENT_DATE)) AS YEAR_MONTH
 FROM AMAZON_SHIPMENT
 GROUP BY YEAR_MONTH;
+/*Find the average number of bathrooms and bedrooms for each city’s property types.
+Output the result along with the city name and the property type.
+COMPANY : AIRBNB
+*/
+SELECT
+    AVG(BATHROOMS),
+    AVG(BEDROOMS),
+    CITY,
+    PROPERTY_TYPE
+FROM AIRBNB_SEARCH_DETAILS
+GROUP BY PROPERTY_TYPE,CITY;
 
+/*Find the most profitable company from the financial sector.
+Output the result along with the continent.
+COMPANY:FORBES
+*/
+
+WITH maxprofits AS
+  (SELECT MAX(profits) AS max_profit
+   FROM forbes_global_2010_2014
+   WHERE sector = 'Financials')
+SELECT company,
+       continent
+FROM forbes_global_2010_2014
+JOIN maxprofits ON forbes_global_2010_2014.profits = maxprofits.max_profit
+WHERE sector = 'Financials';
